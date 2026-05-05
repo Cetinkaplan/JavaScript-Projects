6fa3daee66df8afb352618166b200bfc6bf46952
Selcuk <cetinselcukkaplan@gmail.com>
2026-05-05 18:32:56 -0400

    main_hs_document_writeMethod_implemented_latest

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,34 @@
+
+
+// Window alert method
 window.alert("Hello World  Project1 Expression alert Assigment Working yeyy!");
 
+// write method
+document.write('Document write method Assignment');
 
-document.write('Document write method Assignment')
+// first string
+var X=" This is a String ";
+
+// writing variable
+document.write(X);
+
+// second variable
+var help="This is a variable alert";
+
+// printing variable
+window.alert(help);
+
+//another popup method
+window.alert("She said, \"JavaScript is fun!\"");
+
+
+document.write("This is test of "+ "Concanitaning of String "+ X);
+
+// multiple string in one line
+var Family="Dad", sister="Mehtap",brother="Mehmet",Mommy="Hanim"; // This a variable
+
+//writing multiple string in one shot
+document.write(Family,sister,brother,Mommy); // called variable that has been created before
+
+// writing experession with string
+document.write(3+3 , "This is a expression");
