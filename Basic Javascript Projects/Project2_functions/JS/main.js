dc540700968645f539f37029b20797482560b171
Selcuk <cetinselcukkaplan@gmail.com>
2026-05-06 17:04:26 -0400

    first_FunctionAssignment_concanate

--- Basic Javascript Projects/Project2_functions/JS/main.js
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -3,3 +3,10 @@ var str="This is the button text";
 
 document.getElementById("Button-text").innerHTML=str;
 }
+
+
+function myFirstFunctionConcanate(){
+var str="Second Word with concanate";
+    str+="+ Add second sentence with concanate"
+document.getElementById("Concanate").innerHTML=str;
+}
