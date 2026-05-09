1afe0f5083d28f0351e0a1ef3c425a8aad3297b3
Selcuk <cetinselcukkaplan@gmail.com>
2026-05-09 11:22:36 -0400

    project_3

---
+++ Basic Javascript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,28 @@
+
+
+function math_operator(){
+// + method
+var addition=2+2;
+
+document.getElementById("Math").innerHTML="2+2 ="+addition;
+}
+// Substriction method
+function math_substriction(){
+
+var substriction=5-2;
+
+document.getElementById("Subs").innerHTML="5-2 ="+substriction;
+}
+
+
+// multiple and division method
+
+function multiple_divison(){
+
+var multiple=5*3;
+var division=8/2;
+
+document.getElementById("multiple").innerHTML="5*3 ="+multiple ;
+document.getElementById("division").innerHTML="8/2 ="+division;
+
+}
